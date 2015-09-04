63671fd061ed03eb938e83a3ce86f37230f2c589
thiago h s ramos <thiago.ramos@luizalabs.com>
2015-09-04 00:08:00 -0300

    Work around with filter to translate names of apps

---
+++ material/admin/templatetags/adminfilters.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+The following is a quick little template filter that can be used in the Django admin site to change the app_labels to whatever you like. Override the content/breadcrumb blocks of templates such as admin/index.html, admin/app_index.html, admin/change_form.html, and admin/change_list.html and apply the filter to wherever {{ app.name }} is used.
+"""
+
+### myapp/templatetags/adminfilters.py ###
+
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name='custom_app_label')
+@stringfilter
+def custom_app_label(value):
+    custom_app_labels = {
+        'Auth': 'Autorizações de acesso',
+        'Companies': 'Companhias', 
+        'Visitors': 'Convidados',
+        'Schedules': 'Eventos'# key is default app_label, value is new app_label
+        # Rinse, repeat 
+    }
+    return custom_app_labels.get(value, value)
+
+
